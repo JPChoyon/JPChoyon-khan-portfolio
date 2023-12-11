d30134fbf1ffabdff83a6e1ecee91976f12212c9
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-12-12 00:50:04 +0600

    navbar done

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   const menuItem = (
@@ -8,21 +9,64 @@ const Navbar = () => {
           <a>Home</a>
         </li>
       </NavLink>
+      <NavLink to={"/"}>
+        <li>
+          <a>Project</a>
+        </li>
+      </NavLink>
+      <NavLink to={"/"}>
+        <li>
+          <a>skills</a>
+        </li>
+      </NavLink>
+      <NavLink to={"/"}>
+        <li>
+          <a>Contact Me</a>
+        </li>
+      </NavLink>
     </>
   );
   return (
     <div>
-      <div className="navbar bg-[#79E0EE]">
-        <div className="flex-1 flex gap-4">
-          <img
-            className="h-10"
-            src="https://raw.githubusercontent.com/JPChoyon/JPChoyon-khan-portfolio/main/src/assets/images/logo/logo%20dark%20without%20bg.png"
-            alt="JP Choyon Khan website logo"
-          />
-          <h2 className="font-bold text-xl"> JPCK</h2>
+      <div className="navbar bg-[#79E0EE] flex justify-between">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 uppercase font-bold z-50"
+            >
+              {menuItem}
+            </ul>
+          </div>
+          <div className="flex-1 flex gap-4 items-center">
+            <img
+              className="h-10"
+              src="https://raw.githubusercontent.com/JPChoyon/JPChoyon-khan-portfolio/main/src/assets/images/logo/logo%20dark%20without%20bg.png"
+              alt="JP Choyon Khan website logo"
+            />
+            <h2 className=" text-xl brand-logo"> JPCK</h2>
+          </div>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">{menuItem}</ul>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 uppercase font-bold">
+            {menuItem}
+          </ul>
         </div>
       </div>
     </div>
