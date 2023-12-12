b05829cd637f36fb221dbaae90a867093bdc6c31
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-12-12 20:39:47 +0600

    footer added

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import { FaFacebook, FaInstagramSquare, FaTwitter } from "react-icons/fa";
+import moment from "moment";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="rounded-lg">
+      <div className="md:flex text-center">
+        <div className="footer-left flex-1 py-10">
+          <h2 className="footer-heading">CONTACT US</h2>
+          <p className="footer-pera">
+            Jhenaidah, Khulna, Bangladesh <br /> +88 01987136130 <br />
+            24/7
+          </p>
+        </div>
+        <div className="footer-right flex-1 py-10">
+          <h2 className="footer-heading">Follow US</h2>
+          <p className="footer-pera">Join us on social media</p>
+          <div className="footer-social-icon-container w-40 flex mx-auto  gap-5  text-3xl">
+            <FaFacebook></FaFacebook>
+            <FaInstagramSquare></FaInstagramSquare>
+            <FaTwitter></FaTwitter>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="text-center bg-[#C5FFF8] text-gray-700 py-6 ">
+          Copyright ©JP Choyon Khan. All rights reserved
+          {moment().format(" MMM , YYYY")}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
